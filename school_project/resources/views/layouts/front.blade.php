43efa250bddf18d1254a55ac0b86952b76e2fa26
nosheeniftikhar <nosheeniftikhar00@gmail.com>
2025-06-04 04:52:11 -0700

    updated logout

--- school_project/resources/views/layouts/front.blade.php
+++ school_project/resources/views/layouts/front.blade.php
@@ -97,7 +97,15 @@
                                     <li><a href="#"><i class="flaticon-list"></i>Task</a></li>
                                     <li><a href="#"><i class="flaticon-chat-comment-oval-speech-bubble-with-text-lines"></i>Message</a></li>
                                     <li><a href="#"><i class="flaticon-gear-loading"></i>Account Settings</a></li>
-                                    <li><a href="login.html"><i class="flaticon-turn-off"></i>Log Out</a></li>
+                                    <li><a href="{{ route('logout') }} class="flaticon-turn-off"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </a>
+</li>
                                 </ul>
                             </div>
                         </div>
