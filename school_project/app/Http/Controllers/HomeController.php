acd049a6a57135a75c38634f9e57318385291876
nosheeniftikhar <nosheeniftikhar00@gmail.com>
2025-06-04 23:15:43 -0700

    created student folder
    created student folder.
    created all students controllers: Admission Form, StudentPromotion, AllStudents, StudentDetails.
    
    created Parents controllers:
    AddParent, ParentsDetails, AllParents
    
    Created Teacher Controllers:
    AllTeachers, TeachersDetails, AddTeacher, TeacherPayment

--- school_project/app/Http/Controllers/HomeController.php
+++ school_project/app/Http/Controllers/HomeController.php
@@ -26,8 +26,4 @@ class HomeController extends Controller
         return view('home');
     }
 
-     public function student()
-    {
-        return view('student');
-    }
 }
--- school_project/routes/web.php
+++ school_project/routes/web.php
@@ -20,6 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/student', 'HomeController@student')->name('student');
+Route::get('/student', 'StudentController@student')->name('student');
 Route::get('/teacher', 'TeachersController@teacher')->name('teacher');
 Route::get('/parent', 'ParentsController@parent')->name('parent');
