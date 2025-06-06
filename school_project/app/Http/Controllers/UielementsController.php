dc8703eb361698f29260eea6dd41bc472f1669c4
nosheeniftikhar <nosheeniftikhar00@gmail.com>
2025-06-06 01:13:32 -0700

    created controllers and methods and routes
    I created controllers for all remaining modules and updated their methods and added routes in web.php for all of them

---
+++ school_project/app/Http/Controllers/UielementsController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UielementsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function alart()
+    {
+        return view('ui_elements.notificationalart');
+    }
+    
+    public function button()
+    {
+        return view('ui_elements.button');
+    }
+    
+    public function uitab()
+    {
+        return view('ui_elements.uitab');
+    }
+    
+    public function uiwidget()
+    {
+        return view('ui_elements.uiwidget');
+    }
+    
+    public function modal()
+    {
+        return view('ui_elements.modal');
+    }
+    
+    public function progressBar()
+    {
+        return view('ui_elements.progressbar');
+    }
+
+    public function grid()
+    {
+        return view('ui_elements.grid');
+    }
+
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
